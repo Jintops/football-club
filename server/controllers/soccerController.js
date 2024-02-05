5be0b7a0fe3263440cbf0c38328cffb0d4b06be9
Thomas ps <jintops2003@gmail.com>
2024-02-06 00:30:16 +0530

    finished navbar setup

--- server/controllers/soccerController.js
+++ server/controllers/soccerController.js
@@ -17,5 +17,40 @@ exports.explore=async(req,res)=>{
     res.status(500).send({message:error.message || "error occured"})
 }
 };
-
+exports.about=async(req,res)=>{
+    try{
+     
+       res.render('about')
+    
+}catch(error){
+    res.status(500).send({message:error.message || "error occured"})
+}
+};
+exports.history=async(req,res)=>{
+    try{
+     
+       res.render('realmadrid-history')
+    
+}catch(error){
+    res.status(500).send({message:error.message || "error occured"})
+}
+};
+exports.players=async(req,res)=>{
+    try{
+     
+       res.render('realmadrid-players')
+    
+}catch(error){
+    res.status(500).send({message:error.message || "error occured"})
+}
+};
+exports.stadium=async(req,res)=>{
+    try{
+     
+       res.render('stadium')
+    
+}catch(error){
+    res.status(500).send({message:error.message || "error occured"})
+}
+};
 
--- server/routes/soccerRouters.js
+++ server/routes/soccerRouters.js
@@ -6,5 +6,8 @@ const soccerController = require('../controllers/soccerController');
 
 router.get('/',soccerController.homepage);
 router.get('/exploremore',soccerController.explore)
-
+router.get('/about',soccerController.about)
+router.get('/history',soccerController.history)
+router.get('/players',soccerController.players)
+router.get('/stadium',soccerController.stadium)
 module.exports=router;
